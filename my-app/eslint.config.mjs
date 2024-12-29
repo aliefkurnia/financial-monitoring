6e22b16529ce2305ddbecd268c84f482cfdcb4a7
alipk26 <aliefkw26@gmail.com>
2024-12-29 20:07:25 +0700

    update eslint config

--- my-app/eslint.config.mjs
+++ my-app/eslint.config.mjs
@@ -9,8 +9,18 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
+// Extend the base configuration with Next.js and TypeScript rules
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": "off",
+
+      "@typescript-eslint/no-unused-vars": "off",
+
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
